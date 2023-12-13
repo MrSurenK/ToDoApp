1ab2aff2fd3127a7703ae6fe65a390f47eaec933
MrSurenK <surenkworkz@gmail.com>
2023-12-13 18:01:45 +0800

    Initialised some basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,19 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold">To Do App</h1>
+      <div className="h-screen">
+        <div className="flex flex-col space-y-8 items-center justify-center">
+          <header className="text-7xl mt-80">To Do App</header>
+          <form>
+            <input placeholder="Enter Task"></input>
+            <button className="ml-2 px-4 py-2 bg-blue-700" type="submit">
+              Add
+            </button>
+          </form>
+          <h2>Task 1</h2>
+          <h2>Task 2</h2>
+        </div>
+      </div>
     </>
   );
 }
