4fd0b13b022d340a7d5045a87123e68c2843220f
MrSurenK <surenkworkz@gmail.com>
2023-12-14 22:25:00 +0800

    Fixed some formating

--- src/features/taskList/TaskForm.tsx
+++ src/features/taskList/TaskForm.tsx
@@ -5,7 +5,10 @@ const TaskForm = () => {
     <div className="flex flex-col space-y-8 items-center justify-center">
       <div className="text-7xl mt-60">To Do App</div>
       <form>
-        <input placeholder="Enter Task"></input>
+        <input
+          className=" w-96 border-solid border-sky-500 rounded-md border-2"
+          placeholder="Enter Task"
+        />
         <button className="ml-2 px-4 py-2 bg-blue-700" type="submit">
           Add
         </button>
