42527d5722959d49d161b83bd711d9f5a597c860
MrSurenK <surenkworkz@gmail.com>
2023-12-20 18:33:17 +0800

    fixing styling

--- src/features/taskList/Tasks.tsx
+++ src/features/taskList/Tasks.tsx
@@ -6,25 +6,24 @@ const Tasks = () => {
   const toDo = useSelector(selectTask);
   const dispatch = useDispatch();
   return (
-    <div className="mt-10 flex flex-col items-center justify-center">
-      <div>
+    <div className="mt-10 bg-lime-600 h-screen">
+      <div className="mx-96 bg-blue-500">
         {toDo
           .slice(0) // Creates a shallow copy of the toDo array
           .reverse()
           .map((item, i: number) => {
             if (item.completion === false) {
               return (
-                <div key={i}>
-                  {item.name}
-                  <div className="inline-block ml-10">
-                    <input
-                      name="task"
-                      value="incomplete"
-                      type="checkbox"
-                      onClick={() => dispatch(pending(item.name))}
-                    ></input>
-                  </div>
-                  <div className="inline-block ml-2 bg-red-700 px-2 py-0 text-white shadow-lg hover:shadow-red-950">
+                <div key={i} className="grid grid-cols-4 gap-4">
+                  <span className="col-span-2">{item.name}</span>
+                  <input
+                    name="task"
+                    value="incomplete"
+                    type="checkbox"
+                    onClick={() => dispatch(pending(item.name))}
+                    className="col-span-1"
+                  ></input>
+                  <div className="col-span-1 ml-2 bg-red-700 px-2 py-0 text-white shadow-lg hover:shadow-red-950">
                     <button onClick={() => dispatch(del(toDo.length - 1 - i))}>
                       x
                     </button>
@@ -35,7 +34,7 @@ const Tasks = () => {
               return (
                 <div key={i}>
                   <del>{item.name}</del>
-                  <div className="inline-block">
+                  <div className="inline-block fixed">
                     <input
                       name="task"
                       value="complete"
