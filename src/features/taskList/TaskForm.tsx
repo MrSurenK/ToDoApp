cdcab72699bb5093d99e56de3e159e294d960592
MrSurenK <surenkworkz@gmail.com>
2023-12-20 21:57:48 +0800

    fixed position of delete and checkbox

--- src/features/taskList/TaskForm.tsx
+++ src/features/taskList/TaskForm.tsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import { add } from "./taskFormSlice";
 import { useRef, useState } from "react";
+import Tasks from "./Tasks";
 
 const TaskForm = () => {
   const dispatch = useDispatch();
@@ -11,27 +12,32 @@ const TaskForm = () => {
   return (
     <div className="flex flex-col space-y-8 items-center justify-center">
       <div className="text-7xl mt-60">To Do App</div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          value={userInputVal}
-          onChange={(e) => {
-            setUserInputVal(e.target.value);
-            task.current = e.target.value;
-          }}
-          className=" w-96 border-solid border-sky-500 rounded-full border-2 px-8 py-2 placeholder:text-center text-md"
-          placeholder="Enter Task"
-        />
-        <button
-          className="ml-2 px-4 py-2 bg-blue-700 rounded-full text-white shadow-lg hover:shadow-sky-700"
-          type="submit"
-          onClick={() => {
-            setUserInputVal(" ");
-            dispatch(add(task.current));
-          }}
-        >
-          Add
-        </button>
-      </form>
+      <div className="flex flex-row space-y-4 items-center justify-center">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            value={userInputVal}
+            onChange={(e) => {
+              setUserInputVal(e.target.value);
+              task.current = e.target.value;
+            }}
+            className=" w-96 border-solid border-sky-500 rounded-full border-2 px-8 py-2 placeholder:text-center text-md"
+            placeholder="Enter Task"
+          />
+          <div className="inline-block">
+            <button
+              className="ml-2 px-4 py-2 bg-blue-700 rounded-full text-white shadow-lg hover:shadow-sky-700"
+              type="submit"
+              onClick={() => {
+                setUserInputVal(" ");
+                dispatch(add(task.current));
+              }}
+            >
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+      <Tasks></Tasks>
     </div>
   );
 };
--- src/features/taskList/Tasks.tsx
+++ src/features/taskList/Tasks.tsx
@@ -6,47 +6,47 @@ const Tasks = () => {
   const toDo = useSelector(selectTask);
   const dispatch = useDispatch();
   return (
-    <div className="mt-10 bg-lime-600 h-screen">
-      <div className="mx-96 bg-blue-500">
-        {toDo
-          .slice(0) // Creates a shallow copy of the toDo array
-          .reverse()
-          .map((item, i: number) => {
-            if (item.completion === false) {
-              return (
-                <div key={i} className="grid grid-cols-4 gap-4">
-                  <span className="col-span-2">{item.name}</span>
+    <div className="bg-blue-500 space-y-4 w-96 justify-center flex flex-col">
+      {toDo
+        .slice(0) // Creates a shallow copy of the toDo array
+        .reverse()
+        .map((item, i: number) => {
+          if (item.completion === false) {
+            return (
+              <div key={i} className="bg-purple-500 relative">
+                <span className="">{item.name}</span>
+                <div className="inline-flex absolute right-0">
                   <input
                     name="task"
                     value="incomplete"
                     type="checkbox"
                     onClick={() => dispatch(pending(item.name))}
-                    className="col-span-1"
+                    className=""
                   ></input>
-                  <div className="col-span-1 ml-2 bg-red-700 px-2 py-0 text-white shadow-lg hover:shadow-red-950">
+                  <div className="ml-2 bg-red-700 px-2 py-0 text-white shadow-lg hover:shadow-red-950">
                     <button onClick={() => dispatch(del(toDo.length - 1 - i))}>
                       x
                     </button>
                   </div>
                 </div>
-              );
-            } else {
-              return (
-                <div key={i}>
-                  <del>{item.name}</del>
-                  <div className="inline-block fixed">
-                    <input
-                      name="task"
-                      value="complete"
-                      type="checkbox"
-                      onClick={() => dispatch(pending(item.name))}
-                    ></input>
-                  </div>
+              </div>
+            );
+          } else {
+            return (
+              <div key={i}>
+                <del>{item.name}</del>
+                <div className="inline-block fixed">
+                  <input
+                    name="task"
+                    value="complete"
+                    type="checkbox"
+                    onClick={() => dispatch(pending(item.name))}
+                  ></input>
                 </div>
-              );
-            }
-          })}
-      </div>
+              </div>
+            );
+          }
+        })}
       <Counter />
     </div>
   );
