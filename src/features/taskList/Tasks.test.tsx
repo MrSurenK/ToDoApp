de22ca596300fd95a9e6f6f3644637017ca77023
MrSurenK <surenkworkz@gmail.com>
2023-12-21 22:57:18 +0800

    Pending unit test for UI

--- src/features/taskList/Tasks.test.tsx
+++ src/features/taskList/Tasks.test.tsx
@@ -1,4 +1,5 @@
 import { expect, test } from "vitest";
+import { render, fireEvent, screen } from "@testing-library/react";
 
 import reducer, {
   add,
@@ -6,6 +7,10 @@ import reducer, {
   del,
   TaskListSliceState,
 } from "./taskFormSlice";
+
+import { renderWithProviders } from "../../test/setup";
+import Tasks from "./Tasks";
+import TaskForm from "./TaskForm";
 
 // Run unit test on the reducers
 
@@ -78,3 +83,125 @@ test("should be able to delete selected task from the index input from the todo 
     count: 0,
   });
 });
+
+//Test pending reducer and action to change state
+test("should change completion state to false if true and count should be incremented by 1", () => {
+  const previousState: TaskListSliceState = {
+    task: [
+      {
+        name: "Test Todo",
+        completion: false,
+      },
+      {
+        name: "Second todo",
+        completion: false,
+      },
+    ],
+    count: 0,
+  };
+  expect(reducer(previousState, pending("Test Todo"))).toEqual({
+    task: [
+      {
+        name: "Test Todo",
+        completion: true,
+      },
+      {
+        name: "Second todo",
+        completion: false,
+      },
+    ],
+    count: 1,
+  });
+});
+
+test("should change completed task state back to false if true and decrement count", () => {
+  const previousState: TaskListSliceState = {
+    task: [
+      {
+        name: "Test Todo",
+        completion: true,
+      },
+      {
+        name: "Second todo",
+        completion: false,
+      },
+    ],
+    count: 1,
+  };
+
+  expect(reducer(previousState, pending("Test Todo"))).toEqual({
+    task: [
+      {
+        name: "Test Todo",
+        completion: false,
+      },
+      {
+        name: "Second todo",
+        completion: false,
+      },
+    ],
+    count: 0,
+  });
+});
+
+test("completed task should remain completed when new task added and new task should be given default of false for completion", () => {
+  const previousState1: TaskListSliceState = {
+    task: [
+      {
+        name: "Test Todo",
+        completion: true,
+      },
+      {
+        name: "Second todo",
+        completion: false,
+      },
+    ],
+    count: 1,
+  };
+  expect(reducer(previousState1, pending("Second todo"))).toEqual({
+    task: [
+      {
+        name: "Test Todo",
+        completion: true,
+      },
+      {
+        name: "Second todo",
+        completion: true,
+      },
+    ],
+    count: 2,
+  });
+
+  const previousState2: TaskListSliceState = {
+    task: [
+      {
+        name: "Test Todo",
+        completion: true,
+      },
+      {
+        name: "Second todo",
+        completion: true,
+      },
+    ],
+    count: 2,
+  };
+  expect(reducer(previousState2, add("New Task"))).toEqual({
+    task: [
+      {
+        name: "Test Todo",
+        completion: true,
+      },
+      {
+        name: "Second todo",
+        completion: true,
+      },
+      {
+        name: "New Task",
+        completion: false,
+      },
+    ],
+    count: 2,
+  });
+});
+
+// Test UI
